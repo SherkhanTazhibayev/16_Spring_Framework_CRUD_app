f733fa72943eb3dabe2094a6b7e86c3144cc6627
Sherkhan <tazhibayevsherkhan7@gmail.com>
2022-11-23 23:26:52 +0600

    Merge remote-tracking branch '16_Spring_Framework_CRUD_app/xml_scope' into xml_scope
    # Conflicts:
    #	Spring_CRUD/src/main/webapp/web-inf/views/people/index.html
    #	Spring_CRUD/src/main/webapp/web-inf/views/people/new.html
    #	Spring_CRUD/src/main/webapp/web-inf/views/people/show.html

--- Spring_CRUD/src/main/java/com/example/config/MySpringMvcDispatcherServletInitializer.java
+++ Spring_CRUD/src/main/java/com/example/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,7 +1,11 @@
 package com.example.config;
 
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -18,5 +22,16 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
