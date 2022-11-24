74f804be5afe2bd50ac6656e1efc2d659d8f13d9
Sherkhan <tazhibayevsherkhan7@gmail.com>
2022-11-24 23:52:16 +0600

    Merge remote-tracking branch '16_Spring_Framework_CRUD_app/xml_scope' into xml_scope
    # Conflicts:
    #	Spring_CRUD/src/main/webapp/web-inf/views/people/index.html
    #	Spring_CRUD/src/main/webapp/web-inf/views/people/new.html
    #	Spring_CRUD/src/main/webapp/web-inf/views/people/show.html

--- Spring_CRUD/src/main/java/com/example/controllers/PeopleController.java
+++ Spring_CRUD/src/main/java/com/example/controllers/PeopleController.java
@@ -5,7 +5,13 @@ import com.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+
 
 @Controller
 @RequestMapping("/people")
@@ -23,6 +29,7 @@ public class PeopleController {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
@@ -35,7 +42,11 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -47,7 +58,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         @PathVariable("id") int id,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
--- Spring_CRUD/src/main/java/com/example/models/Person.java
+++ Spring_CRUD/src/main/java/com/example/models/Person.java
@@ -1,8 +1,25 @@
 package com.example.models;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "name should not be empty")
+    @Size(min = 2, max = 30, message = "name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 12, message = "age should be greater than 12")
+    private int age;
+
+    @NotEmpty(message = "email should not be empty")
+    @Email(message = "email should be valid")
+    private String email;
 
     public Person(){
 
@@ -27,6 +44,22 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
 }
