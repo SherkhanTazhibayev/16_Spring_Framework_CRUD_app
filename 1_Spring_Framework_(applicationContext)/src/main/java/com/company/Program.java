60a42b567f7320d29e11545a00b97fced6cc7f9b
Sherkhan <tazhibayevsherkhan7@gmail.com>
2022-12-10 23:40:23 +0600

    rename "TestProgram" to "Class_Get_Bean"

--- 1_Spring_Framework_(applicationContext)/src/main/java/com/company/Program.java
+++ 1_Spring_Framework_(applicationContext)/src/main/java/com/company/Program.java
@@ -4,13 +4,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Program {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Class_Get_Bean class_get_bean = context.getBean("get_bean_from_xml", Class_Get_Bean.class);
+        Bean class_get_bean = context.getBean("get_bean_from_xml", Bean.class);
 
-        System.out.println(class_get_bean.getName());
+//        System.out.println(class_get_bean.getName());
 
         context.close();
     }
